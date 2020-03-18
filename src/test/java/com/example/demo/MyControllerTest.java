3e9ff7bb6015788ae83d24abd708d2e840166362
rlwing <rob@wingjr.com>
2020-03-18 09:34:11 -0700

    initial commit

---
+++ src/test/java/com/example/demo/MyControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MyControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void sayHello() throws Exception {
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk());
+    }
+}
